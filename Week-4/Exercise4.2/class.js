e7a30097ba0c2168f7cfc6ef5ad3e3f3784de0a3
Jay <jay05thakare@gmail.com>
2022-09-15 12:04:18 +0530

    added teacher class and set teacher prototype to person

--- Week-4/Exercise4.2/class.js
+++ Week-4/Exercise4.2/class.js
@@ -1,16 +1,19 @@
-var Person = function () {};
+var Person = function () {name: 'jay'};
 
- Person.prototype.initialize = function (name, age) {
+Person.prototype.initialize = function (name, age) {
   this.name = name;
   this.age = age;
 };
 
 // TODO: create the class Teacher and a method teach
 function Teacher() {
-  teach: this.teach;
-}
+  teach = function(subject) {
+    return console.log(this.name + 'is now teaching' + subject);
+  }
+};
 
 Object.setPrototypeOf(Teacher, Person);
+console.log(Object.getPrototypeOf(Teacher));
 
 var him = new Teacher();
 
