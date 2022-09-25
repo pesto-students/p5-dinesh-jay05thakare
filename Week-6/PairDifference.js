a1e9ef035ccc40aedfa0d2561d83a161765f45b0
Jay <jay05thakare@gmail.com>
2022-09-25 17:51:46 +0530

    formatting

--- Week-6/PairDifference.js
+++ Week-6/PairDifference.js
@@ -1,19 +1,17 @@
-const A = [10, 1, 7, 2, 1, 16, 80];
+const A = [10, 1, 7, 80, 2, 1, 16];
 const B = 78;
-
 
 let x = B + A[0];
 
-for(let el of A){
-
-  if(A.includes(x)){
+for (let el of A) {
+  if (A.includes(x)) {
     console.log(1);
     return 1;
-  }else{
-    x = B + el
+  } else {
+    x = B + el;
   }
 
-  if(el == A[A.length - 1]){
+  if (el == A[A.length - 1]) {
     console.log(0);
   }
 }
